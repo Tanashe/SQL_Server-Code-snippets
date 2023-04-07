78e3d04bee15ee9318c25ca5e0ff0d87c892f588
Tanashe <45070343+Tanashe@users.noreply.github.com>
2023-04-07 15:00:59 -0400

    Add files via upload

---
+++ SQLSERVER project.sql
@@ -0,0 +1,22 @@
+--Creating EmployeeDemographics Table
+CREATE TABLE EmployeeDemographics
+(EmployeeID int,
+FirstName varchar(50),
+LastName varchar(50),
+Age int,
+Gender varchar(50)
+)
+
+
+--Inserting Multiple rows  into EmployeeDemographics table
+INSERT INTO EmployeeDemographics values
+(1001, 'Rufaro', 'Chaga', 37, 'Female'),
+(1002, 'Kim', 'Parker', 26, 'Female'),
+(1003, 'Dell', 'Samma', 31, 'Male'),
+(1004, 'Rodd', 'Tee', 39, 'Male'),
+(1005, 'Tino', 'Kamba', 30 ,'Female'),
+(1006, 'Heather', 'Black', 29, 'Female'),
+(1007, 'Don', 'Tillman', 35, 'Male'),
+(1008, 'Yana', 'Drues', 37, 'Female'),
+(1009, 'Sam', 'Muti', 28, 'Male'),
+(1010, 'Rue', 'Gee', 32, 'Female')
